93c3749364c84933e2ad5d12729b33fa82b4c27f
jirayu-phan <jirayu.phan@ku.th>
2026-07-31 17:27:45 +0700

    Add files via upload

--- BoundedStack.java
+++ BoundedStack.java
@@ -5,12 +5,14 @@ public class BoundedStack {
 
     /*  
       Abstraction Function:
-      AF(items, capacity) = สแตกที่มีสมาชิกคือสมาชิกทั้งหมดใน items โดยยอดสแตกอยู่ที่ items.get(0)
-      เเละยอดสเเตก (ตัวบนสุด) อยู่ที่ items.get(items.size() - 1)
+      AF(items, capacity) = สแตกที่มีสมาชิกคือสมาชิกทั้งหมดใน items 
+      โดยยอด Stack อยู่ที่ items.get(0)
+      เเละยอด Stack (ตัวบนสุด) อยู่ที่ items.get(items.size() - 1)
 
       Representation Invariant:
-        - items != null
+        - items != null;
         - items.size() <= capacity;
+        - capacity >= 0;
         
     */
        private final List<String> items;
@@ -18,7 +20,7 @@ public class BoundedStack {
     
     public BoundedStack(int capacity) {
         if (capacity < 0) {                      
-            throw new IllegalArgumentException("capacity must be >= 0");
+            throw new IllegalArgumentException("capacity >= 0");
         }
         this.capacity = capacity;
         this.items = new ArrayList<>();
@@ -26,58 +28,90 @@ public class BoundedStack {
     }
 
     private void checkRep() {
-        assert items != null : "items not null";
-        assert items.size() <= capacity ;  
-        assert capacity >= 0 : "Capacity must be >= 0" ;
+        if (items == null) {
+        throw new AssertionError("items == null");
+        }
+        if (capacity < 0) {
+            throw new AssertionError("capacity < 0");
+        }
+        if (items.size() > capacity) {
+            throw new AssertionError("items <= capacity");
+        }
     }
 
     /**
-     * @param 
-     * @throws 
+     * @param x รับค่าเข้ามา ค่าที่ต้องการเพิ่มเข้า Stack
+     * @throws IllegalArgumentException เมื่อ x เป็น Null
+     * @throws IllegalStateException เมื่อสเเตกเต็มเเล้ว
      */
-    public void push(Object x) {
-        
-        
-        
+    public void push(String x) {
+        if (x == null) {
+            throw new IllegalArgumentException();
+        }
+        if (items.size() >= capacity) {
+            throw new IllegalStateException();
+        }
+        items.add(x);
+        checkRep();
     }
 
     /**
-     * @return 
-     * @throws 
+     * pop ทำการลบค่าตำเเหน่งสุดท้ายออก เเละ คืนค่าตำเเหน่งสุดท้าย
+     * @return ค่่าตำเเหล่งสุดท้าย
+     * @throws IllegalStateException เมื่อค่าว่าง ไม่มีอะไรให้ pop
      */
-    public Object pop() {
-        
-        
-        
-        return null; // แก้ตามจริง
+    public String pop() {
+        if (items.isEmpty()) {
+            throw new IllegalStateException();
+        }
+        String top = items.get(items.size() - 1);
+        items.remove(items.size() -1);
+        checkRep();
+        return top;
     }
 
-    public Object peek() {
-        
-        return null;
+
+    /**
+     * @throws IllegalStateException เมื่อค่าว่าง
+     * @return ค่าตำเเหน่งสุดท้าย
+     */
+    public String peek() {
+        if (items.isEmpty()) {
+            throw new IllegalStateException();
+        }
+        return items.get(items.size() -1);
     }
 
-   
+    /*
+     *  @return คืนค่า true เมื่อไม่มีสมาชิกเลย
+    */
     public boolean isEmpty() {
-        
-        return false;
+        return items.isEmpty();
     }
 
-    
+    /*
+     *  @return คืนค่า true เมื่อสมาชิกมีค่าเท่ากับ capacity
+    */
     public boolean isFull() {
-       
-        return false;
+        return items.size() == capacity ;
     }
 
-   
+    /*
+     * @return คืนค่าจำนวนสมาชิกทั้งหมด
+    */
     public int size() {
-       
-        return -1;
+        return items.size();
     }
 
+    /*
+     * Producer Stack มีสมาชิกเเละลำดับเหมือนกัน เเต่คนละ Object
+     * @return คืนค่า Stack ใหม่ที่มีสมาชิกเเละ capacity 
+    */
     public BoundedStack copy() {
-       
-        return null;
+        BoundedStack stack = new BoundedStack(capacity);
+        for (String s : items)
+            stack.push(s);
+        return stack;
     }
 
 }
